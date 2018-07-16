8e916dc7f76e62e10e8d06f63e8b79b8dcf168ab
Luis Pineda JR <lspnd00@gmail.com>
2018-07-16 12:47:49 -0400

    controlled uncontrolled input

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <h3>{this.state.input}</h3>
         <input type='text' onChange={this.updateInput} value={this.state.input} />
         <input type='text' ref={(input)=> this.text = input}/>
         <button onClick={this.submit}>Show / Hide</button>
