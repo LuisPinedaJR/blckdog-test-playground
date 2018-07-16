8222c2b36272855ef233b477a6cf8d5a12054d69
Luis Pineda JR <lspnd00@gmail.com>
2018-07-16 13:04:25 -0400

    logo - header

--- src/App.js
+++ src/App.js
@@ -3,48 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  state = {
-    input: 'Hello'
-  }
-
-  updateInput = (event) => {
-    console.log(event.target.value)
-    this.setState({
-      input: event.target.value.trim()
-    })
-  }
-
-  submit = () =>{
-    console.log(this.text.value)
-    }
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Welcome text="Welcome to Blck Dog Digital Playground"/>
+          <img src={logo} className="App-logo" alt="logo" /> 
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h3>{this.state.input}</h3>
-        <input type='text' onChange={this.updateInput} value={this.state.input} />
-        <input type='text' ref={(input)=> this.text = input}/>
-        <button onClick={this.submit}>Show / Hide</button>
-
       </div>
     );
   }
 }
 
-class Welcome extends Component{
-  render(){
-    const { text } = this.props;
-    return(
-      <h1 className="App-title">{text}</h1>
-    )
-  }
-}
 
 export default App;
